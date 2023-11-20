500c9502cd429dec5ba3135017219cdb692bab30
Ironman616 <150930956+Ironman616@users.noreply.github.com>
2023-11-20 21:13:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/VJ-FILTER-BOT.git /VJ-FILTER-BOT 
+  git clone https://github.com/Ironman616/MOVIE-LOVERZ-bot.git /MOVIE-LOVERZ-bot 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /VJ-FILTER-BOT 
+  git clone $UPSTREAM_REPO /MOVIE-LOVERZ-bot 
 fi
-cd /VJ-FILTER-BOT 
+cd /MOVIE-LOVERZ-bot 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
