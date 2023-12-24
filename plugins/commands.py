30fd622a27dfe3b32e58752099297bff5dfb54b5
Ironman616 <150930956+Ironman616@users.noreply.github.com>
2023-12-25 02:14:44 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -82,7 +82,7 @@ async def start(client, message):
             return
         btn = [
             [
-                InlineKeyboardButton("❆ Jᴏɪɴ Oᴜʀ Cʜᴀɴɴᴇʟ ❆", url='t.me/MOVIE_LOVERZZ')
+                InlineKeyboardButton("❆ Jᴏɪɴ Oᴜʀ Cʜᴀɴɴᴇʟ ❆", url= invite_link.invite_link)
             ],[
                 InlineKeyboardButton('🤔 Why Iam Join🤔', callback_data='sinfo')
             ]
