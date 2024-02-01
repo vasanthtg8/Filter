741df6497123dc84414df212002c450f5481199d
vasanthtg8 <131646876+vasanthtg8@users.noreply.github.com>
2024-02-01 16:51:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -118,9 +118,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://moviesuniverse.onrender.com/".format(FQDN)
+    URL = "https://moviesuniverse-825c494655ce.herokuapp.com/".format(FQDN)
 else:
-    URL = "https://moviesuniverse.onrender.com/".format(FQDN)
+    URL = "https://moviesuniverse-825c494655ce.herokuapp.com/".format(FQDN)
 
 
 
